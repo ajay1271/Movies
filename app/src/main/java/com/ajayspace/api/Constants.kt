1c0c1afe99b975c08c1d921dde5ad9be87bc7d4e
ajay1271 <ajayanurag.05@gmail.com>
2022-01-06 22:48:59 +0530

    initial

--- app/src/main/java/com/ajayspace/api/Constants.kt
+++ app/src/main/java/com/ajayspace/api/Constants.kt
@@ -2,7 +2,7 @@ package com.ajayspace.api
 
 object Constants {
 
-    const val API_KEY = "2bfc2845a8e711f212828a7f8d23d3a7"
+    const val API_KEY = "ADD_YOUR_KEY"
 
     const val BACKDROP_IMG_BASE_URL = "https://image.tmdb.org/t/p/original/"
     const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342/"
