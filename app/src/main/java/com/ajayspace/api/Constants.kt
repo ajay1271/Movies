37cfc9d27750c91d431b2709052110b8b52de120
ajay1271 <ajayanurag.05@gmail.com>
2022-01-07 19:09:55 +0530

    Update Constants.kt
    Added API key

--- app/src/main/java/com/ajayspace/api/Constants.kt
+++ app/src/main/java/com/ajayspace/api/Constants.kt
@@ -2,7 +2,7 @@ package com.ajayspace.api
 
 object Constants {
 
-    const val API_KEY = "YOUR_API_KEY"
+    const val API_KEY = "13c1fcbeab2cdeaf26c49ee1a6c842a8"
 
     const val BACKDROP_IMG_BASE_URL = "https://image.tmdb.org/t/p/original/"
     const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342/"
